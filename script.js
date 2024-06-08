58d972027af0350c857def02abd8d83724176571
rushithorat <163113533+rushithorat@users.noreply.github.com>
2024-06-08 16:49:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var rushi = new Swiper(".myrushi", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
